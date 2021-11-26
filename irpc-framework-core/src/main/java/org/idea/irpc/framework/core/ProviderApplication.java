636139432de13c589651b685de40417697e39de0
linhao <hao.lin2@zhenai.com>
2021-11-26 21:58:55 +0800

    初始化课程内容

---
+++ irpc-framework-core/src/main/java/org/idea/irpc/framework/core/ProviderApplication.java
@@ -0,0 +1,12 @@
+package org.idea.irpc.framework.core;
+
+/**
+ * @Author linhao
+ * @Date created in 9:57 下午 2021/11/26
+ */
+public class ProviderApplication {
+
+    public static void main(String[] args) {
+        System.out.println("first lab");
+    }
+}
